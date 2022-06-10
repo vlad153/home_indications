00db79c8c0470bc818ed9f06e7d46f1faca46155
Vlad <vlad_ss1994@mail.ru>
2022-06-10 16:33:58 +0300

    add output

--- home_indication/router/route.py
+++ home_indication/router/route.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, File
 
 import cv2 as cv
 import pytesseract
+from pytesseract import Output
 
 image_router = APIRouter()
 
@@ -10,5 +11,5 @@ image_router = APIRouter()
 async def recognize_image(file: bytes = File()):
     image_array = np.asarray(bytearray(file), dtype="uint8")
     image = cv.imdecode(image_array, cv.IMREAD_COLOR)
-    data_image = pytesseract.image_to_data(image)
+    data_image = pytesseract.image_to_data(image, output_type=Output.DICT)
     return data_image
